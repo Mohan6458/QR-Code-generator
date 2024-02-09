0e8e1a0ca2649f3fe0c17a62d16e455c5401af39
Mohana Krishnan Ramesh <mohanakrishnan8670@gmail.com>
2024-02-09 15:05:58 +0000

    Small QR Code generator project using NodeJs

---
+++ index.js
@@ -0,0 +1,39 @@
+import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fs from 'fs';
+
+
+inquirer
+  .prompt([
+    {
+     message: "type in the URL : ",
+     name : "URL",
+    },
+   ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+
+    fs.writeFile('URL.txt', url, (err) => {
+        if (err) throw err;
+        console.log('QR image has been generated!');
+      }); COPY
+      
+
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
+/* 
+1. Use the inquirer npm package to get user input.
+2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+3. Create a txt file to save the user input using the native fs node module.
+*/
+
+
